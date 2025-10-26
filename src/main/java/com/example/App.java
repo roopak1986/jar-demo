b7180ae24fcac88feab6e3e4bf66abb3602753b1
roopak1986 <roopak.tyagi001@gmail.com>
2025-10-26 10:33:09 +0530

    next jenkins updated

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from JAR jenkins demo!");
+        System.out.println("Hello from JAR jenkins demo V1!");
     }
 }
